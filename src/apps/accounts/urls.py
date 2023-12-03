7f584e8320eb5eeecda57d355b7ffabe35a9ca53
Patrick Wayodi <patrickwayodi@gmail.com>
2023-12-04 02:13:00 +0300

    Add files via upload

--- src/apps/accounts/urls.py
+++ src/apps/accounts/urls.py
@@ -1,1 +1,12 @@
+from django.shortcuts import render
+from django.urls import path
+from django.views.generic.base import TemplateView
 
+from .views import SignUpView
+
+urlpatterns = [
+
+    path("signup/", SignUpView.as_view(), name="signup"),
+    path("", TemplateView.as_view(template_name="accounts/home.html"), name="home"),
+
+]
