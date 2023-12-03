110d62a5f98c3e53f39a63900f4327ca75899b5e
Patrick Wayodi <patrickwayodi@gmail.com>
2023-12-04 02:06:00 +0300

    Add files via upload

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,1 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic.base import TemplateView
 
+urlpatterns = [
+    path("", include('apps.accounts.urls')),
+    path("admin/", admin.site.urls),
+    path("accounts/", include("apps.accounts.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),     
+    path("", include('apps.assets.urls')),
+]
