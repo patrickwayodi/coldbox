00f7532977ff45dcecc529872a7836698135a3dc
Patrick Wayodi <patrickwayodi@gmail.com>
2023-12-05 22:21:10 +0300

    Add files via upload

--- src/apps/assets/urls.py
+++ src/apps/assets/urls.py
@@ -5,7 +5,6 @@ from django.shortcuts import render
 from django.urls import path, include
 from django.views.generic.base import TemplateView
 
-
 from .views import CreateAssetView, AssetDetailView, HomeView
 
 
